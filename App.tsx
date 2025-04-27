ae5766c57d30fe5e18cf860bf7472e621adbf0ff
JoseCob <josecobkantun123@gmail.com>
2025-04-26 22:17:01 -0600

    Animación añadido
    Se agrego la animación de carga del inicio de la aplicación

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
+import React, { useState } from 'react';
+import LoadScreen from './src/presentation/screens/LoadScreen';
 import AppNavigator from './src/presentation/navigation/AppNavigator';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false); //Establecemos la pantalla de carga en falso
+
+  //Aqui hacemos que inicie la animación de carga cuando se ejecute la apliación
+  if (!isReady) {
+    return <LoadScreen onFinish={() => setIsReady(true)} />;
+  }
+
   return <AppNavigator />;
 }
--- src/presentation/screens/LoadScreen.tsx
+++ src/presentation/screens/LoadScreen.tsx
@@ -1,13 +1,60 @@
 //Aqui se creara una animacion de carga para el HomeScreen con Reanimated 3
-import React from "react";
-import {View, Text} from "react-native";
+import React, {useEffect} from "react";
+import {View, Text, StyleSheet} from "react-native";
+import Animated, {useSharedValue, useAnimatedStyle, withTiming, Easing, runOnJS} from "react-native-reanimated";
 
-const LoadScreen = () => {
+//Función que controla la llamada de la acción para la animación
+type Props = {
+    onFinish: () => void;
+};
+
+//Funcion que espera la propiedad OnFinish para ejecutar la animación en HomeScreen
+const LoadScreen:React.FC<Props> = ({onFinish}) => {
+    const scale = useSharedValue(0.5);
+    const opacity = useSharedValue(0);
+
+    //Se construye la animación
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [{ scale: scale.value }],
+        opacity: opacity.value,
+      }));
+
+      //Se establece el tiempo(duración) de la animación
+      useEffect(() => {
+        opacity.value = withTiming(1, { duration: 800 });
+        scale.value = withTiming(1, {
+          duration: 2500,
+          easing: Easing.out(Easing.exp), //Animación que la detiene suavemente
+        }, () => {
+          runOnJS(onFinish)(); //Aseguramos que se ejecute la animación de forma correcta y evitar posibles errores
+        });
+      }, []);
+
     return(
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Animated.View style={[styles.box, animatedStyle]}>
+                <Text style={styles.text}>🚀</Text>
+            </Animated.View>
         </View>
     );
 }
 
-export default LoadScreen
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#121212',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    box: {
+      backgroundColor: '#1f1f1f',
+      padding: 30,
+      borderRadius: 20,
+    },
+    text: {
+      color: '#fff',
+      fontSize: 24,
+    },
+});
+
+export default LoadScreen;
